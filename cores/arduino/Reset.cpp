973ccc47d8fbad115343ea0b74e005509e9bc19b
Thibaut VIARD <thibaut.viard@atmel.com>
2014-04-15 14:41:32 +0200

    Adding first files, not Zero specific

---
+++ cores/arduino/Reset.cpp
@@ -0,0 +1,57 @@
+/*
+  Copyright (c) 2012 Arduino.  All right reserved.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  See the GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include <Arduino.h>
+#include "Reset.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+__attribute__ ((long_call, section (".ramfunc")))
+void banzai() {
+	// Disable all interrupts
+	__disable_irq();
+
+  // Reset the device
+	// todo
+
+	while (true);
+}
+
+static int ticks = -1;
+
+void initiateReset(int _ticks) {
+	ticks = _ticks;
+}
+
+void cancelReset() {
+	ticks = -1;
+}
+
+void tickReset() {
+	if (ticks == -1)
+		return;
+	ticks--;
+	if (ticks == 0)
+		banzai();
+}
+
+#ifdef __cplusplus
+}
+#endif
