554364a21b7c9cd661e433e48fb13101a23ae451
Thibaut VIARD <thibaut.viard@atmel.com>
2014-04-24 18:25:20 +0200

    First draft for Wiring.c

--- cores/arduino/wiring.c
+++ cores/arduino/wiring.c
@@ -22,81 +22,51 @@
 extern "C" {
 #endif
 
-uint32_t millis( void )
-{
-// todo: ensure no interrupts
-    return GetTickCount() ;
-}
-
-// Interrupt-compatible version of micros
-// Theory: repeatedly take readings of SysTick counter, millis counter and SysTick interrupt pending flag.
-// When it appears that millis counter and pending is stable and SysTick hasn't rolled over, use these 
-// values to calculate micros. If there is a pending SysTick, add one to the millis counter in the calculation.
-uint32_t micros( void )
-{
-    uint32_t ticks, ticks2;
-    uint32_t pend, pend2;
-    uint32_t count, count2;
-
-    ticks2  = SysTick->VAL;
-    pend2   = !!((SCB->ICSR & SCB_ICSR_PENDSTSET_Msk)||((SCB->SHCSR & SCB_SHCSR_SYSTICKACT_Msk)))  ;
-    count2  = GetTickCount();
-
-    do {
-        ticks=ticks2;
-        pend=pend2;
-        count=count2;
-        ticks2  = SysTick->VAL;
-        pend2   = !!((SCB->ICSR & SCB_ICSR_PENDSTSET_Msk)||((SCB->SHCSR & SCB_SHCSR_SYSTICKACT_Msk)))  ;
-        count2  = GetTickCount();
-    } while ((pend != pend2) || (count != count2) || (ticks < ticks2));
-
-    return ((count+pend) * 1000) + (((SysTick->LOAD  - ticks)*(1048576/(F_CPU/1000000)))>>20) ; 
-    // this is an optimization to turn a runtime division into two compile-time divisions and 
-    // a runtime multiplication and shift, saving a few cycles
-}
-
-// original function:
-// uint32_t micros( void )
-// {
-//     uint32_t ticks ;
-//     uint32_t count ;
-// 
-//     SysTick->CTRL;
-//     do {
-//         ticks = SysTick->VAL;
-//         count = GetTickCount();
-//     } while (SysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk);
-// 
-//     return count * 1000 + (SysTick->LOAD + 1 - ticks) / (SystemCoreClock/1000000) ;
-// }
-
-
-void delay( uint32_t ms )
-{
-    if (ms == 0)
-        return;
-    uint32_t start = GetTickCount();
-    do {
-        yield();
-    } while (GetTickCount() - start < ms);
-}
-
-#if defined ( __ICCARM__ ) /* IAR Ewarm 5.41+ */
-extern signed int putchar( signed int c ) ;
-/**
- * \brief
- *
- * \param c  Character to output.
- *
- * \return The character that was output.
+/*
+ * System Core Clock is at 1MHz at Reset.
+ * It is switched to 48MHz in the Reset Handler (startup.c)
  */
-extern WEAK signed int putchar( signed int c )
+extern uint32_t SystemCoreClock=1000000ul ;
+
+void __libc_init_array(void);
+
+/*
+ * Arduino Zero board initialization
+ *
+ * Good to know:
+ *   - Watchdog is disabled by default, unless someone plays with NVM User page
+ *   -
+ */
+void init( void )
 {
-    return c ;
+  // Set Systick to 1ms interval, common to all SAM3 variants
+  if (SysTick_Config(SystemCoreClock / 1000))
+  {
+    // Capture error
+    while (true);
+  }
+
+
+  // Initialize C library
+//  __libc_init_array();
+
+  // Disable pull-up on every pin
+  for ( int i = 0 ; i < PINS_COUNT ; i++ )
+	{
+	  digitalWrite( i, LOW ) ;
+	}
+
+  // Initialize Serial port U(S)ART pins
+	// Todo
+
+  // Initialize USB pins
+	// Todo
+
+  // Initialize Analog Controller
+	// Todo
 }
-#endif /* __ICCARM__ */
 
 #ifdef __cplusplus
 }
 #endif
+
