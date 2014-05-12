493c1c817e0903c0b98ab096e532d7db95f23b2f
Thibaut VIARD <thibaut.viard@atmel.com>
2014-05-12 12:06:45 +0200

    Adding reset pin configuration (input/pullup)

--- cores/arduino/validation/test.cpp
+++ cores/arduino/validation/test.cpp
@@ -81,7 +81,7 @@ void loop( void )
   Serial.write( "test2" ) ;   // send another string
 */
 
-  // Test digitalRead: connect pin 2 to either GND or 3.3V. !!!! NOT on 5V pinZZ !!!!
+  // Test digitalRead: connect pin 2 to either GND or 3.3V. !!!! NOT on 5V pin !!!!
   pin_value=digitalRead( 2 ) ;
 //  Serial.write( "pin 2 value is %s\n", (pin_value == LOW)?"LOW":"HIGH" ) ;
   delay( 1000 ) ;              // wait for a second
