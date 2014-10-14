9348def3296d8c4d36a24d790bff6456edaa2b0d
Thibaut VIARD <thibaut.viard@atmel.com>
2014-10-14 16:15:39 +0200

    Removing useless header include
    Signed-off-by: Thibaut VIARD <thibaut.viard@atmel.com>

--- cores/arduino/wiring_digital.c
+++ cores/arduino/wiring_digital.c
@@ -16,10 +16,8 @@
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-//#include "Arduino.h"
 #include "wiring_digital.h"
 #include "WVariant.h"
-//#include "Arduino.h"
 
 #ifdef __cplusplus
  extern "C" {
