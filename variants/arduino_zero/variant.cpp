1a08dd4a505a1be66d39dff793475e814f99cf6a
Thibaut VIARD <thibaut.viard@atmel.com>
2014-11-20 14:31:45 +0100

    Fixing Serialx pins mismatch
    Signed-off-by: Thibaut VIARD <thibaut.viard@atmel.com>

--- variants/arduino_zero/variant.cpp
+++ variants/arduino_zero/variant.cpp
@@ -207,5 +207,5 @@ SERCOM sercom3( SERCOM3 ) ;
 SERCOM sercom4( SERCOM4 ) ;
 SERCOM sercom5( SERCOM5 ) ;
 
-Uart Serial1( &sercom0, PIN_SERIAL_RX, PIN_SERIAL_TX ) ;
+Uart Serial1( &sercom0, PIN_SERIAL1_RX, PIN_SERIAL1_TX ) ;
 Uart Serial( &sercom5, PIN_SERIAL_RX, PIN_SERIAL_TX ) ;
