7b14e6df2045891d6a9abc11c9db1817a84aece7
Cristian Maglie <c.maglie@arduino.cc>
2014-11-28 09:51:42 +0100

    fixed indent in Uart.h Uart.cpp

--- cores/arduino/Uart.cpp
+++ cores/arduino/Uart.cpp
@@ -22,14 +22,14 @@
 
 Uart::Uart(SERCOM *_s, uint8_t _pinRX, uint8_t _pinTX)
 {
-	sercom = _s;
-	uc_pinRX = _pinRX;
-	uc_pinTX = _pinTX;
+  sercom = _s;
+  uc_pinRX = _pinRX;
+  uc_pinTX = _pinTX;
 }
 
 void Uart::begin(unsigned long baudrate)
 {
-	begin(baudrate, (uint8_t)SERIAL_8N1);
+  begin(baudrate, (uint8_t)SERIAL_8N1);
 }
 
 void Uart::begin(unsigned long baudrate, uint8_t config)
@@ -37,129 +37,129 @@ void Uart::begin(unsigned long baudrate, uint8_t config)
   pinPeripheral(uc_pinRX, g_APinDescription[uc_pinRX].ulPinType);
   pinPeripheral(uc_pinTX, g_APinDescription[uc_pinTX].ulPinType);
 
-	sercom->initUART(UART_INT_CLOCK, SAMPLE_RATE_x16, baudrate);
-	sercom->initFrame(extractCharSize(config), LSB_FIRST, extractParity(config), extractNbStopBit(config));
-	sercom->initPads(UART_TX_PAD_2, SERCOM_RX_PAD_3);
+  sercom->initUART(UART_INT_CLOCK, SAMPLE_RATE_x16, baudrate);
+  sercom->initFrame(extractCharSize(config), LSB_FIRST, extractParity(config), extractNbStopBit(config));
+  sercom->initPads(UART_TX_PAD_2, SERCOM_RX_PAD_3);
 
 
-	sercom->enableUART();
+  sercom->enableUART();
 }
 
 void Uart::end()
 {
-	sercom->resetUART();
-	rxBuffer.clear();
+  sercom->resetUART();
+  rxBuffer.clear();
 }
 
 void Uart::flush()
 {
-	sercom->flushUART();
+  sercom->flushUART();
 }
 
 void Uart::IrqHandler()
 {
-	if(sercom->availableDataUART())
-	{
-		rxBuffer.store_char(sercom->readDataUART());
-	}
+  if(sercom->availableDataUART())
+  {
+    rxBuffer.store_char(sercom->readDataUART());
+  }
 
-	if(	sercom->isBufferOverflowErrorUART() ||
-		sercom->isFrameErrorUART() ||
-		sercom->isParityErrorUART())
-	{
-		sercom->clearStatusUART();
-	}
+  if(  sercom->isBufferOverflowErrorUART() ||
+    sercom->isFrameErrorUART() ||
+    sercom->isParityErrorUART())
+  {
+    sercom->clearStatusUART();
+  }
 }
 
 int Uart::available()
 {
-	return rxBuffer.available();
+  return rxBuffer.available();
 }
 
 int Uart::peek()
 {
-	return rxBuffer.peek();
+  return rxBuffer.peek();
 }
 
 int Uart::read()
 {
-	return rxBuffer.read_char();
+  return rxBuffer.read_char();
 }
 
 size_t Uart::write(const uint8_t data)
 {
-	sercom->writeDataUART(data);
-	return 1;
+  sercom->writeDataUART(data);
+  return 1;
 }
 
 size_t Uart::write(const char * data)
 {
-	size_t writed = 0;
+  size_t writed = 0;
 
-	while(*data != '\0')
-	{
-		writed += write(*data);
-		++data;
-	}
+  while(*data != '\0')
+  {
+    writed += write(*data);
+    ++data;
+  }
 
-	return writed;
+  return writed;
 }
 
 SercomNumberStopBit Uart::extractNbStopBit(uint8_t config)
 {
-	switch(config & HARDSER_STOP_BIT_MASK)
-	{
-		case HARDSER_STOP_BIT_1:
-		default:
-			return SERCOM_STOP_BIT_1;
+  switch(config & HARDSER_STOP_BIT_MASK)
+  {
+    case HARDSER_STOP_BIT_1:
+    default:
+      return SERCOM_STOP_BIT_1;
 
-		case HARDSER_STOP_BIT_2:
-			return SERCOM_STOP_BITS_2;
-	}
+    case HARDSER_STOP_BIT_2:
+      return SERCOM_STOP_BITS_2;
+  }
 }
 
 SercomUartCharSize Uart::extractCharSize(uint8_t config)
 {
-	switch(config & HARDSER_DATA_MASK)
-	{
-		case HARDSER_DATA_5:
-			return UART_CHAR_SIZE_5_BITS;
+  switch(config & HARDSER_DATA_MASK)
+  {
+    case HARDSER_DATA_5:
+      return UART_CHAR_SIZE_5_BITS;
 
-		case HARDSER_DATA_6:
-			return UART_CHAR_SIZE_6_BITS;
+    case HARDSER_DATA_6:
+      return UART_CHAR_SIZE_6_BITS;
 
-		case HARDSER_DATA_7:
-			return UART_CHAR_SIZE_7_BITS;
+    case HARDSER_DATA_7:
+      return UART_CHAR_SIZE_7_BITS;
 
-		case HARDSER_DATA_8:
-		default:
-			return UART_CHAR_SIZE_8_BITS;
+    case HARDSER_DATA_8:
+    default:
+      return UART_CHAR_SIZE_8_BITS;
 
-	}
+  }
 }
 
 SercomParityMode Uart::extractParity(uint8_t config)
 {
-	switch(config & HARDSER_PARITY_MASK)
-	{
-		case HARDSER_PARITY_NONE:
-		default:
-			return SERCOM_NO_PARITY;
+  switch(config & HARDSER_PARITY_MASK)
+  {
+    case HARDSER_PARITY_NONE:
+    default:
+      return SERCOM_NO_PARITY;
 
-		case HARDSER_PARITY_EVEN:
-			return SERCOM_EVEN_PARITY;
+    case HARDSER_PARITY_EVEN:
+      return SERCOM_EVEN_PARITY;
 
-		case HARDSER_PARITY_ODD:
-			return SERCOM_ODD_PARITY;
-	}
+    case HARDSER_PARITY_ODD:
+      return SERCOM_ODD_PARITY;
+  }
 }
 
 void SERCOM0_Handler()
 {
-	Serial1.IrqHandler();
+  Serial1.IrqHandler();
 }
 
 void SERCOM5_Handler()
 {
-	Serial.IrqHandler();
+  Serial.IrqHandler();
 }
--- cores/arduino/Uart.h
+++ cores/arduino/Uart.h
@@ -28,32 +28,32 @@
 
 class Uart : public HardwareSerial
 {
-	public:
-		Uart(SERCOM *_s, uint8_t _pinRX, uint8_t _pinTX);
-		void begin(unsigned long baudRate);
-		void begin(unsigned long baudrate, uint8_t config);
-		void end();
-		int available();
-		int peek();
-		int read();
-		void flush();
-		size_t write(const uint8_t data);
-		size_t write(const char * data);
+  public:
+    Uart(SERCOM *_s, uint8_t _pinRX, uint8_t _pinTX);
+    void begin(unsigned long baudRate);
+    void begin(unsigned long baudrate, uint8_t config);
+    void end();
+    int available();
+    int peek();
+    int read();
+    void flush();
+    size_t write(const uint8_t data);
+    size_t write(const char * data);
 
-		void IrqHandler();
+    void IrqHandler();
 
-		operator bool() { return true; }
+    operator bool() { return true; }
 
-	private:
-		SERCOM *sercom;
-		RingBuffer rxBuffer;
+  private:
+    SERCOM *sercom;
+    RingBuffer rxBuffer;
 
     uint8_t uc_pinRX;
     uint8_t uc_pinTX;
 
-		SercomNumberStopBit extractNbStopBit(uint8_t config);
-		SercomUartCharSize extractCharSize(uint8_t config);
-		SercomParityMode extractParity(uint8_t config);
+    SercomNumberStopBit extractNbStopBit(uint8_t config);
+    SercomUartCharSize extractCharSize(uint8_t config);
+    SercomParityMode extractParity(uint8_t config);
 };
 
 extern Uart Serial;
