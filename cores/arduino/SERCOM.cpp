12b3774c3f757d8619c99a2ba78fbf6cace18076
Sandeep Mistry <s.mistry@arduino.cc>
2015-10-23 09:08:46 -0400

    Use reg instead of bit to enable/disable the DRE interrupt
    The RXC interrupt was being disabled when using bit to disable the DRE
    interrupt.

--- cores/arduino/SERCOM.cpp
+++ cores/arduino/SERCOM.cpp
@@ -175,12 +175,12 @@ int SERCOM::writeDataUART(uint8_t data)
 
 void SERCOM::enableDataRegisterEmptyInterruptUART()
 {
-  sercom->USART.INTENSET.bit.DRE = 1;
+  sercom->USART.INTENSET.reg |= SERCOM_USART_INTENSET_DRE;
 }
 
 void SERCOM::disableDataRegisterEmptyInterruptUART()
 {
-  sercom->USART.INTENCLR.bit.DRE = 1;
+  sercom->USART.INTENCLR.reg = SERCOM_USART_INTENCLR_DRE;
 }
 
 /*	=========================
