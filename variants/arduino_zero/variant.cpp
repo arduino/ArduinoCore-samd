d8f4306d0f5c94951bb761b267480623fd0b9239
ladyada <limor@ladyada.net>
2016-01-23 19:41:31 -0500

    Serial5 not available anyhow

--- variants/arduino_zero/variant.cpp
+++ variants/arduino_zero/variant.cpp
@@ -208,14 +208,18 @@ SERCOM sercom4( SERCOM4 ) ;
 SERCOM sercom5( SERCOM5 ) ;
 
 Uart Serial1( &sercom0, PIN_SERIAL1_RX, PIN_SERIAL1_TX, PAD_SERIAL1_RX, PAD_SERIAL1_TX ) ;
-Uart Serial5( &sercom5, PIN_SERIAL_RX, PIN_SERIAL_TX, PAD_SERIAL_RX, PAD_SERIAL_TX ) ;
 void SERCOM0_Handler()
 {
   Serial1.IrqHandler();
 }
 
+
+// Serial5 not available on Feather so ditch it
+
+//Uart Serial5( &sercom5, PIN_SERIAL_RX, PIN_SERIAL_TX, PAD_SERIAL_RX, PAD_SERIAL_TX ) ;
+/*
 void SERCOM5_Handler()
 {
   Serial5.IrqHandler();
 }
-
+*/
