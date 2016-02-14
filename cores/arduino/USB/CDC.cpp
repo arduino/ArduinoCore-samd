ae249718d44cf6b901a4cde2d75449ce2949bdb9
ladyada <limor@ladyada.net>
2016-02-14 15:37:58 -0500

    add USB CDC RX & TX LEDs with 10ms minimum on time, tested

--- cores/arduino/USB/CDC.cpp
+++ cores/arduino/USB/CDC.cpp
@@ -23,6 +23,8 @@
 #include <stdio.h>
 #include <stdint.h>
 
+uint8_t USB_TXLEDticks = 0, USB_RXLEDticks = 0;
+
 #ifdef CDC_ENABLED
 
 #define CDC_SERIAL_BUFFER_SIZE	256
@@ -91,6 +93,12 @@ bool CDC_Setup(USBSetup& setup)
 {
 	uint8_t requestType = setup.bmRequestType;
 	uint8_t r = setup.bRequest;
+
+	digitalWrite(PIN_LED_RXL, HIGH);
+	digitalWrite(PIN_LED_TXL, HIGH);
+	pinMode(PIN_LED_RXL, OUTPUT);
+	pinMode(PIN_LED_TXL, OUTPUT);
+	USB_TXLEDticks = USB_RXLEDticks = 0;
 
 	if (requestType == REQUEST_DEVICETOHOST_CLASS_INTERFACE)
 	{
@@ -176,10 +184,16 @@ int Serial_::peek(void)
 
 int Serial_::read(void)
 {
-	if (_serialPeek != -1) {
-		int res = _serialPeek;
-		_serialPeek = -1;
-		return res;
+	ring_buffer *buffer = &cdc_rx_buffer;
+
+	digitalWrite(PIN_LED_RXL, LOW);
+	USB_RXLEDticks = 10; // how many ms to keep LED on 
+
+	// if the head isn't ahead of the tail, we don't have any characters
+	if (buffer->head == buffer->tail && !buffer->full)
+	{
+		if (usb.available(CDC_ENDPOINT_OUT))
+			accept();
 	}
 	return usb.recv(CDC_ENDPOINT_OUT);
 }
@@ -205,7 +219,23 @@ void Serial_::flush(void)
 
 size_t Serial_::write(const uint8_t *buffer, size_t size)
 {
-	uint32_t r = usb.send(CDC_ENDPOINT_IN, buffer, size);
+	/* only try to send bytes if the high-level CDC connection itself
+	 is open (not just the pipe) - the OS should set lineState when the port
+	 is opened and clear lineState when the port is closed.
+	 bytes sent before the user opens the connection or after
+	 the connection is closed are lost - just like with a UART. */
+
+        digitalWrite(PIN_LED_TXL, LOW);
+	USB_TXLEDticks = 10; // how many ms to keep LED on 
+
+	// TODO - ZE - check behavior on different OSes and test what happens if an
+	// open connection isn't broken cleanly (cable is yanked out, host dies
+	// or locks up, or host virtual serial port hangs)
+	uint32_t r = 0;
+	if (_usbLineInfo.lineState > 0)  // Problem with Windows(R)
+	{
+		r = usb.send(CDC_ENDPOINT_IN, buffer, size);
+	}
 
 	if (r > 0) {
 		return r;
@@ -243,54 +273,6 @@ Serial_::operator bool()
 	return result;
 }
 
-<<<<<<< HEAD
-int32_t Serial_::readBreak() {
-	uint8_t enableInterrupts = ((__get_PRIMASK() & 0x1) == 0);
-
-	// disable interrupts,
-	// to avoid clearing a breakValue that might occur 
-	// while processing the current break value
-	__disable_irq();
-
-	int32_t ret = breakValue;
-
-	breakValue = -1;
-
-	if (enableInterrupts) {
-		// re-enable the interrupts
-		__enable_irq();
-	}
-
-	return ret;
-}
-
-unsigned long Serial_::baud() {
-	return _usbLineInfo.dwDTERate;
-}
-
-uint8_t Serial_::stopbits() {
-	return _usbLineInfo.bCharFormat;
-}
-
-uint8_t Serial_::paritytype() {
-	return _usbLineInfo.bParityType;
-}
-
-uint8_t Serial_::numbits() {
-	return _usbLineInfo.bDataBits;
-}
-
-bool Serial_::dtr() {
-	return _usbLineInfo.lineState & 0x1;
-}
-
-bool Serial_::rts() {
-	return _usbLineInfo.lineState & 0x2;
-}
-
-Serial_ SerialUSB(USBDevice);
-=======
 Serial_ Serial(USBDevice);
->>>>>>> make it so native USB is Serial
 
 #endif
