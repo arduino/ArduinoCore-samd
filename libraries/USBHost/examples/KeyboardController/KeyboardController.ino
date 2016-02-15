54cc0b272505598f7c71c5054c7e86beef4f223a
ladyada <limor@ladyada.net>
2016-02-14 19:26:57 -0500

    Working demo with nice status change notifications

--- libraries/USBHost/examples/KeyboardController/KeyboardController.ino
+++ libraries/USBHost/examples/KeyboardController/KeyboardController.ino
@@ -2,7 +2,7 @@
  Keyboard Controller Example
 
  Shows the output of a USB Keyboard connected to
- the Native USB port on an Arduino Due Board.
+ the Native USB port on an Arduino Zero
 
  created 8 Oct 2012
  by Cristian Maglie
@@ -15,6 +15,13 @@
 // Require keyboard control library
 #include <KeyboardController.h>
 
+
+// on a zero with debug port, use debug port
+//#define SerialDebug Serial
+
+// on a feather or non-debug Zero, use Serial1 (since USB is taken!)
+#define SerialDebug Serial1
+
 // Initialize USB Controller
 USBHost usb;
 
@@ -25,60 +32,62 @@ void printKey();
 
 // This function intercepts key press
 void keyPressed() {
-  SERIAL_PORT_MONITOR.print("Pressed:  ");
+  SerialDebug.print("Pressed:  ");
   printKey();
 }
 
 // This function intercepts key release
 void keyReleased() {
-  SERIAL_PORT_MONITOR.print("Released: ");
+  SerialDebug.print("Released: ");
   printKey();
 }
 
 void printKey() {
   // getOemKey() returns the OEM-code associated with the key
-  SERIAL_PORT_MONITOR.print(" key:");
-  SERIAL_PORT_MONITOR.print(keyboard.getOemKey());
+  SerialDebug.print(" key:");
+  SerialDebug.print(keyboard.getOemKey());
 
   // getModifiers() returns a bits field with the modifiers-keys
   int mod = keyboard.getModifiers();
-  SERIAL_PORT_MONITOR.print(" mod:");
-  SERIAL_PORT_MONITOR.print(mod);
+  SerialDebug.print(" mod:");
+  SerialDebug.print(mod);
 
-  SERIAL_PORT_MONITOR.print(" => ");
+  SerialDebug.print(" => ");
 
   if (mod & LeftCtrl)
-    SERIAL_PORT_MONITOR.print("L-Ctrl ");
+    SerialDebug.print("L-Ctrl ");
   if (mod & LeftShift)
-    SERIAL_PORT_MONITOR.print("L-Shift ");
+    SerialDebug.print("L-Shift ");
   if (mod & Alt)
-    SERIAL_PORT_MONITOR.print("Alt ");
+    SerialDebug.print("Alt ");
   if (mod & LeftCmd)
-    SERIAL_PORT_MONITOR.print("L-Cmd ");
+    SerialDebug.print("L-Cmd ");
   if (mod & RightCtrl)
-    SERIAL_PORT_MONITOR.print("R-Ctrl ");
+    SerialDebug.print("R-Ctrl ");
   if (mod & RightShift)
-    SERIAL_PORT_MONITOR.print("R-Shift ");
+    SerialDebug.print("R-Shift ");
   if (mod & AltGr)
-    SERIAL_PORT_MONITOR.print("AltGr ");
+    SerialDebug.print("AltGr ");
   if (mod & RightCmd)
-    SERIAL_PORT_MONITOR.print("R-Cmd ");
+    SerialDebug.print("R-Cmd ");
 
   // getKey() returns the ASCII translation of OEM key
   // combined with modifiers.
-  SERIAL_PORT_MONITOR.write(keyboard.getKey());
-  SERIAL_PORT_MONITOR.println();
+  SerialDebug.write(keyboard.getKey());
+  SerialDebug.println();
 }
+
+uint32_t lastUSBstate = 0;
 
 void setup()
 {
-  SERIAL_PORT_MONITOR.begin( 115200 );
-  while (!SERIAL_PORT_MONITOR); // Wait for serial port to connect - used on Leonardo, Teensy and other boards with built-in USB CDC serial connection
-  SERIAL_PORT_MONITOR.println("Keyboard Controller Program started");
+  SerialDebug.begin( 115200 );
+  SerialDebug.println("USB Host Keyboard Controller Program started");
 
   if (usb.Init() == -1)
-	  SERIAL_PORT_MONITOR.println("OSC did not start.");
-  
+	  SerialDebug.println("USB Host did not start.");
+
+  SerialDebug.println("USB Host started");
   delay( 20 );
 }
 
@@ -86,4 +95,21 @@ void loop()
 {
   // Process USB tasks
   usb.Task();
+
+  uint32_t currentUSBstate = usb.getUsbTaskState();
+  if (lastUSBstate != currentUSBstate) {
+    SerialDebug.print("USB state changed: 0x"); 
+    SerialDebug.print(lastUSBstate, HEX); 
+    SerialDebug.print(" -> 0x"); 
+    SerialDebug.println(currentUSBstate, HEX);
+    switch (currentUSBstate) {
+      case USB_ATTACHED_SUBSTATE_SETTLE: SerialDebug.println("Device Attached"); break;
+      case USB_DETACHED_SUBSTATE_WAIT_FOR_DEVICE: SerialDebug.println("Detached, waiting for Device"); break;
+      case USB_ATTACHED_SUBSTATE_RESET_DEVICE: SerialDebug.println("Resetting Device"); break;
+      case USB_ATTACHED_SUBSTATE_WAIT_RESET_COMPLETE: SerialDebug.println("Reset complete"); break;
+      case USB_STATE_CONFIGURING: SerialDebug.println("USB Configuring"); break;
+      case USB_STATE_RUNNING: SerialDebug.println("USB Running"); break;
+    }
+    lastUSBstate = currentUSBstate;
+  }
 }
