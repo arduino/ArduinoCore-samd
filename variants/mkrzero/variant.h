78619131d231fbad3f025f9ef310fc8e6a6e9eaa
Sandeep Mistry <s.mistry@arduino.cc>
2016-11-28 12:03:01 -0500

    MKRZero: Add I2S info to variant.h

--- variants/mkrzero/variant.h
+++ variants/mkrzero/variant.h
@@ -141,6 +141,16 @@ static const uint8_t SCK1  = PIN_SPI1_SCK;
 #define PIN_USB_DP          (23ul)
 #define PIN_USB_HOST_ENABLE (24ul)
 
+// I2S Interfaces
+// --------------
+#define I2S_INTERFACES_COUNT 1
+
+#define I2S_DEVICE          0
+#define I2S_CLOCK_GENERATOR 3
+#define PIN_I2S_SD          (PIN_A6)
+#define PIN_I2S_SCK         (2u)
+#define PIN_I2S_FS          (3u)
+
 // Serial ports
 // ------------
 #ifdef __cplusplus
