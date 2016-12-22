95c9ec6fb0e1d0679de3d42e3b62264219ba541e
Cristian Maglie <c.maglie@arduino.cc>
2016-12-22 18:21:32 +0100

    Added bootloader and boards definition for MKRFox1200 board

--- bootloaders/zero/build_all_bootloaders.sh
+++ bootloaders/zero/build_all_bootloaders.sh
@@ -13,5 +13,8 @@ mv -v samd21_sam_ba_genuino_mkr1000.* ../mkr1000/
 BOARD_ID=arduino_mkrzero NAME=samd21_sam_ba_arduino_mkrzero make clean all
 mv -v samd21_sam_ba_arduino_mkrzero.* ../mkrzero/
 
+BOARD_ID=arduino_mkrfox1200 NAME=samd21_sam_ba_arduino_mkrfox1200 make clean all
+mv -v samd21_sam_ba_arduino_mkrfox1200.* ../mkrfox1200/
+
 echo Done building bootloaders!
 
