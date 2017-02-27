fbc2a138b40c4d810417a3a4f08fb8de6b4ac660
ladyada <limor@ladyada.net>
2017-02-27 16:12:03 -0500

    bigger buffer for Wire

--- cores/arduino/RingBuffer.h
+++ cores/arduino/RingBuffer.h
@@ -25,7 +25,7 @@
 // using a ring buffer (I think), in which head is the index of the location
 // to which to write the next incoming character and tail is the index of the
 // location from which to read.
-#define SERIAL_BUFFER_SIZE 64
+#define SERIAL_BUFFER_SIZE 164
 
 class RingBuffer
 {
