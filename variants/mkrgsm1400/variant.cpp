04ebeee38d2cd45519f15acff7c02fbefbed1287
Sandeep Mistry <s.mistry@bcmi-labs.cc>
2017-08-31 11:50:40 -0400

    Use RTS and CTS inside MODEM

--- variants/mkrgsm1400/variant.cpp
+++ variants/mkrgsm1400/variant.cpp
@@ -209,10 +209,6 @@ void initVariant() {
   // Workaround for RTS not being controlled correctly
   pinMode(GSM_RTS, OUTPUT);
   digitalWrite(GSM_RTS, LOW);
-
-  // Hold reset high to prevent cellular modem from starting if not used
-  pinMode(GSM_RESETN, OUTPUT);
-  digitalWrite(GSM_RESETN, HIGH);
 }
 
 // Serial1
