6a44349ebb4dd72e28dd0737e983ca3a48403b0b
mitchellp <mitchell.pontague@gmail.com>
2017-11-21 08:15:54 +0800

    Revert encoding on I2C

--- cores/arduino/SERCOM.cpp
+++ cores/arduino/SERCOM.cpp
@@ -370,7 +370,7 @@ void SERCOM::enableWIRE()
 {
   // I2C Master and Slave modes share the ENABLE bit function.
 
-  // Enable the IÂ²C master mode
+  // Enable the I²C master mode
   sercom->I2CM.CTRLA.bit.ENABLE = 1 ;
 
   while ( sercom->I2CM.SYNCBUSY.bit.ENABLE != 0 )
@@ -391,7 +391,7 @@ void SERCOM::disableWIRE()
 {
   // I2C Master and Slave modes share the ENABLE bit function.
 
-  // Enable the IÂ²C master mode
+  // Enable the I²C master mode
   sercom->I2CM.CTRLA.bit.ENABLE = 0 ;
 
   while ( sercom->I2CM.SYNCBUSY.bit.ENABLE != 0 )
