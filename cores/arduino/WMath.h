ce568182e18f15b1252d13ad5c6281de603e9cd9
Helmut Tschemernjak <helmut@helios.de>
2017-11-27 17:03:44 +0100

    Removed macro map parameters to avoid build errors

--- cores/arduino/WMath.h
+++ cores/arduino/WMath.h
@@ -28,7 +28,7 @@ extern uint16_t makeWord( uint16_t w ) ;
 extern uint16_t makeWord( uint8_t h, uint8_t l ) ;
 
 #define word(...) makeWord(__VA_ARGS__)
-#define map(a,b,c,d,e)  Arduino_map(a,b,c,d,e)
+#define map	Arduino_map
 
 
 #endif /* _WIRING_MATH_ */
