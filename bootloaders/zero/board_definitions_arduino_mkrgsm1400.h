6306c5db0a4514d8a45211bfc14194c98a37f4ef
Sandeep Mistry <s.mistry@bcmi-labs.cc>
2017-11-30 10:08:56 -0500

    Make some USB product strings consistent

--- bootloaders/zero/board_definitions_arduino_mkrgsm1400.h
+++ bootloaders/zero/board_definitions_arduino_mkrgsm1400.h
@@ -22,7 +22,7 @@
 /*
  * USB device definitions
  */
-#define STRING_PRODUCT "Arduino MKRGSM1400"
+#define STRING_PRODUCT "Arduino MKR GSM 1400"
 #define USB_VID_HIGH   0x23
 #define USB_VID_LOW    0x41
 #define USB_PID_HIGH   0x00
