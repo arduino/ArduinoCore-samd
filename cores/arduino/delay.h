9b30cd8a59898f8c7c63863136b221dae4217512
dean <deanm1278@gmail.com>
2018-04-02 13:06:02 -0400

    DM: fix delay microseconds

--- cores/arduino/delay.h
+++ cores/arduino/delay.h
@@ -68,6 +68,20 @@ static __inline__ void delayMicroseconds( unsigned int usec )
     return ;
   }
 
+#if defined(__SAMD51__)
+  uint32_t n = usec * (VARIANT_MCK / 1000000) / 12;
+
+  __asm__ __volatile__(
+    "1:              \n"
+    "   sub %0, #1   \n" // substract 1 from %0 (n)
+    "   cmp %0, #0   \n" // compare to 0
+    "   bne 1b       \n" // if result is not 0 jump to 1
+    : "+r" (n)           // '%0' is n variable with RW constraints
+    :                    // no input
+    :                    // no clobber
+  );
+
+#else
   /*
    *  The following loop:
    *
@@ -85,6 +99,7 @@ static __inline__ void delayMicroseconds( unsigned int usec )
   // VARIANT_MCK / 1000000 == cycles needed to delay 1uS
   //                     3 == cycles used in a loop
   uint32_t n = usec * (VARIANT_MCK / 1000000) / 3;
+
   __asm__ __volatile__(
     "1:              \n"
     "   sub %0, #1   \n" // substract 1 from %0 (n)
@@ -93,6 +108,7 @@ static __inline__ void delayMicroseconds( unsigned int usec )
     :                    // no input
     :                    // no clobber
   );
+#endif
   // https://gcc.gnu.org/onlinedocs/gcc/Extended-Asm.html
   // https://gcc.gnu.org/onlinedocs/gcc/Extended-Asm.html#Volatile
 }
