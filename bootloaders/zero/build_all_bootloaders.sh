64fddfa44a833a1f440a4beadbae32ba8fcbcadf
Martino Facchin <m.facchin@arduino.cc>
2018-04-10 17:54:46 +0200

    VIDOR: add proper bootloader

--- bootloaders/zero/build_all_bootloaders.sh
+++ bootloaders/zero/build_all_bootloaders.sh
@@ -25,5 +25,8 @@ mv -v samd21_sam_ba_arduino_mkrwan1300.* ../mkrwan1300/
 BOARD_ID=arduino_mkrwifi1010 NAME=samd21_sam_ba_arduino_mkrwifi1010 make clean all
 mv -v samd21_sam_ba_arduino_mkrwifi1010.* ../mkrwifi1010/
 
+BOARD_ID=arduino_mkrvidor2000 NAME=samd21_sam_ba_arduino_mkrvidor make clean all
+mv -v samd21_sam_ba_arduino_mkrvidor.* ../mkrvidor2000/
+
 echo Done building bootloaders!
 
