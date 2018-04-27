e8531f84f6b5d35165d078f0549aab904dc82c04
ladyada <limor@ladyada.net>
2018-04-27 11:40:26 -0400

    the evil DR MERTO strikes again! fixes https://github.com/adafruit/ArduinoCore-samd/issues/38

--- variants/metro_m4/variant.h
+++ variants/metro_m4/variant.h
@@ -16,8 +16,8 @@
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-#ifndef _VARIANT_MERTO_M4_
-#define _VARIANT_MERTO_M4_
+#ifndef _VARIANT_METRO_M4_
+#define _VARIANT_METRO_M4_
 
 // The definitions here needs a SAMD core >=1.6.10
 #define ARDUINO_SAMD_VARIANT_COMPLIANCE 10610
@@ -267,5 +267,5 @@ extern Uart Serial1;
 #define SERIAL_PORT_HARDWARE        Serial1
 #define SERIAL_PORT_HARDWARE_OPEN   Serial1
 
-#endif /* _VARIANT_MERTO_M4_ */
+#endif /* _VARIANT_METRO_M4_ */
 
