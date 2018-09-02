9ac80d9a3af64ae45adfef43db6dc8dba1186b88
gdsports <gdsports256@gmail.com>
2018-09-01 15:42:15 -1000

    Fix memory overwrite in USBHost
    Fix issue #271.

--- cores/arduino/USB/samd21_host.c
+++ cores/arduino/USB/samd21_host.c
@@ -147,10 +147,7 @@ void UHD_Init(void)
 	USB->HOST.DESCADD.reg = (uint32_t)(&usb_pipe_table[0]);
 	// For USB_SPEED_FULL
 	uhd_force_full_speed();
-	for (i = 0; i < sizeof(usb_pipe_table); i++)
-	{
-		(*(uint32_t *)(&usb_pipe_table[0] + i)) = 0;
-	}
+	memset((void *)usb_pipe_table, 0, sizeof(usb_pipe_table));
 
 	uhd_state = UHD_STATE_NO_VBUS;
 
