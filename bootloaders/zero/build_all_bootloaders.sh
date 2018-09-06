0399f7faec0909d54e97761d9fa95e5e37199916
Bart Meijer <bart@reprapworld.com>
2018-09-06 14:46:54 +0200

    Added support for Minitronics v2.0

--- bootloaders/zero/build_all_bootloaders.sh
+++ bootloaders/zero/build_all_bootloaders.sh
@@ -25,5 +25,9 @@ mv -v samd21_sam_ba_arduino_mkrwan1300.* ../mkrwan1300/
 BOARD_ID=arduino_mkrwifi1010 NAME=samd21_sam_ba_arduino_mkrwifi1010 make clean all
 mv -v samd21_sam_ba_arduino_mkrwifi1010.* ../mkrwifi1010/
 
+BOARD_ID=reprapworld_minitronics20 NAME=samd21_sam_ba_reprapworld_minitronics20 make clean all
+mv -v samd21_sam_ba_reprapworld_minitronics20.* ../minitronics20/
+
+
 echo Done building bootloaders!
 
