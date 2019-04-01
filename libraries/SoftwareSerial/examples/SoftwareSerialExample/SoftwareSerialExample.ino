6b847464bcd3cb8bd0e52117757f53d2f7a9fced
per1234 <accounts@perglass.com>
2019-04-01 11:06:34 +0200

    Update libraries/SoftwareSerial/examples/SoftwareSerialExample/SoftwareSerialExample.ino
    Co-Authored-By: agdl <a.guadalupi@arduino.cc>

--- libraries/SoftwareSerial/examples/SoftwareSerialExample/SoftwareSerialExample.ino
+++ libraries/SoftwareSerial/examples/SoftwareSerialExample/SoftwareSerialExample.ino
@@ -31,7 +31,7 @@ SoftwareSerial mySerial(10, 11); // RX, TX
 
 void setup() {
   // Open serial communications and wait for port to open:
-  Serial.begin(57600);
+  Serial.begin(9600);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
