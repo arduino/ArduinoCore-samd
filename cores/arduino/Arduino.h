3991f0acf34ff16e0df9b13d89ad11da3ae6e811
Noah <noah@koontzs.com>
2019-04-11 09:01:37 -0700

    Replaced `using` declarations with custom implementation
    Created multi-type template for min and max.

--- cores/arduino/Arduino.h
+++ cores/arduino/Arduino.h
@@ -1,17 +1,14 @@
 /*
   Arduino.h - Main include file for the Arduino SDK
   Copyright (c) 2014 Arduino LLC.  All right reserved.
-
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
   License as published by the Free Software Foundation; either
   version 2.1 of the License, or (at your option) any later version.
-
   This library is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
   Lesser General Public License for more details.
-
   You should have received a copy of the GNU Lesser General Public
   License along with this library; if not, write to the Free Software
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
@@ -104,8 +101,17 @@ void loop( void ) ;
 #define abs(x) ((x)>0?(x):-(x))
 #define round(x)     ((x)>=0?(long)((x)+0.5):(long)((x)-0.5))
 #else
-  using std::min;
-  using std::max;
+  template<class T, class L> 
+  auto min(const T& a, const L& b) -> decltype((b < a) ? b : a)
+  {
+    return (b < a) ? b : a;
+  }
+
+  template<class T, class L> 
+  auto max(const T& a, const L& b) -> decltype((b < a) ? b : a)
+  {
+    return (a < b) ? b : a;
+  }
 #endif
 
 #define constrain(amt,low,high) ((amt)<(low)?(low):((amt)>(high)?(high):(amt)))
