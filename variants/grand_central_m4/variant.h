2bf7821f70d71700f6e9c43429dabd1bcc573590
GMagician <GMagician@users.noreply.github.com>
2019-07-26 21:56:08 +0200

    Missing pin

--- variants/grand_central_m4/variant.h
+++ variants/grand_central_m4/variant.h
@@ -81,50 +81,50 @@ extern "C"
 // #define digitalPinToTimer(P)
 
 // LEDs
-#define PIN_LED_13           (13)
-#define PIN_LED_RXL          (75)
-#define PIN_LED_TXL          (76)
-#define PIN_LED              PIN_LED_13
-#define PIN_LED2             PIN_LED_RXL
-#define PIN_LED3             PIN_LED_TXL
-#define LED_BUILTIN          PIN_LED_13
-#define PIN_NEOPIXEL		 (88u)
+#define PIN_LED_13          (13)
+#define PIN_LED_RXL         (75)
+#define PIN_LED_TXL         (76)
+#define PIN_LED             PIN_LED_13
+#define PIN_LED2            PIN_LED_RXL
+#define PIN_LED3            PIN_LED_TXL
+#define LED_BUILTIN         PIN_LED_13
+#define PIN_NEOPIXEL		    (88)
 
 /*
  * Analog pins
  */
-#define PIN_A0               (67)
-#define PIN_A1               (PIN_A0 + 1)
-#define PIN_A2               (PIN_A0 + 2)
-#define PIN_A3               (PIN_A0 + 3)
-#define PIN_A4               (PIN_A0 + 4)
-#define PIN_A5               (PIN_A0 + 5)
-#define PIN_A6               (PIN_A0 + 6)
-#define PIN_A7               (PIN_A0 + 7)
+#define PIN_A0              (67)
+#define PIN_A1              (PIN_A0 + 1)
+#define PIN_A2              (PIN_A0 + 2)
+#define PIN_A3              (PIN_A0 + 3)
+#define PIN_A4              (PIN_A0 + 4)
+#define PIN_A5              (PIN_A0 + 5)
+#define PIN_A6              (PIN_A0 + 6)
+#define PIN_A7              (PIN_A0 + 7)
 
-#define PIN_A8               (54)
-#define PIN_A9               (PIN_A8 + 1)
-#define PIN_A10              (PIN_A8 + 2)
-#define PIN_A11              (PIN_A8 + 3)
-#define PIN_A12              (PIN_A8 + 4)
-#define PIN_A13              (PIN_A8 + 5)
-#define PIN_A14              (PIN_A8 + 6)
-#define PIN_A15              (PIN_A8 + 7)
+#define PIN_A8              (54)
+#define PIN_A9              (PIN_A8 + 1)
+#define PIN_A10             (PIN_A8 + 2)
+#define PIN_A11             (PIN_A8 + 3)
+#define PIN_A12             (PIN_A8 + 4)
+#define PIN_A13             (PIN_A8 + 5)
+#define PIN_A14             (PIN_A8 + 6)
+#define PIN_A15             (PIN_A8 + 7)
 
-#define PIN_DAC0             PIN_A0
-#define PIN_DAC1             PIN_A1
+#define PIN_DAC0            PIN_A0
+#define PIN_DAC1            PIN_A1
 
-static const uint8_t A0  = PIN_A0;
-static const uint8_t A1  = PIN_A1;
-static const uint8_t A2  = PIN_A2;
-static const uint8_t A3  = PIN_A3;
-static const uint8_t A4  = PIN_A4;
-static const uint8_t A5  = PIN_A5;
-static const uint8_t A6  = PIN_A6;
-static const uint8_t A7  = PIN_A7;
+static const uint8_t A0   = PIN_A0;
+static const uint8_t A1   = PIN_A1;
+static const uint8_t A2   = PIN_A2;
+static const uint8_t A3   = PIN_A3;
+static const uint8_t A4   = PIN_A4;
+static const uint8_t A5   = PIN_A5;
+static const uint8_t A6   = PIN_A6;
+static const uint8_t A7   = PIN_A7;
 
-static const uint8_t A8  = PIN_A8;
-static const uint8_t A9  = PIN_A9;
+static const uint8_t A8   = PIN_A8;
+static const uint8_t A9   = PIN_A9;
 static const uint8_t A10  = PIN_A10;
 static const uint8_t A11  = PIN_A11;
 static const uint8_t A12  = PIN_A12;
@@ -135,10 +135,10 @@ static const uint8_t A15  = PIN_A15;
 static const uint8_t DAC0 = PIN_DAC0;
 static const uint8_t DAC1 = PIN_DAC1;
 
-#define ADC_RESOLUTION		12
+#define ADC_RESOLUTION		  12
 
 // Other pins
-#define PIN_ATN              (39)
+#define PIN_ATN             (39)
 static const uint8_t ATN = PIN_ATN;
 
 /*
@@ -146,55 +146,55 @@ static const uint8_t ATN = PIN_ATN;
  */
 
 // Serial1
-#define PIN_SERIAL1_RX       (0)
-#define PIN_SERIAL1_TX       (1)
-#define PAD_SERIAL1_TX       (UART_TX_PAD_0)
-#define PAD_SERIAL1_RX       (SERCOM_RX_PAD_1)
+#define PIN_SERIAL1_RX      (0)
+#define PIN_SERIAL1_TX      (1)
+#define PAD_SERIAL1_TX      (UART_TX_PAD_0)
+#define PAD_SERIAL1_RX      (SERCOM_RX_PAD_1)
 
 // Serial2
-#define PIN_SERIAL2_RX       (19)
-#define PIN_SERIAL2_TX       (18)
-#define PAD_SERIAL2_TX       (UART_TX_PAD_0)
-#define PAD_SERIAL2_RX       (SERCOM_RX_PAD_1)
-#define SERCOM_SERIAL2		 sercom4
+#define PIN_SERIAL2_RX      (19)
+#define PIN_SERIAL2_TX      (18)
+#define PAD_SERIAL2_TX      (UART_TX_PAD_0)
+#define PAD_SERIAL2_RX      (SERCOM_RX_PAD_1)
+#define SERCOM_SERIAL2		  sercom4
 
 // Serial3
-#define PIN_SERIAL3_RX       (17)
-#define PIN_SERIAL3_TX       (16)
-#define PAD_SERIAL3_TX       (UART_TX_PAD_0)
-#define PAD_SERIAL3_RX       (SERCOM_RX_PAD_1)
-#define SERCOM_SERIAL3		 sercom1
+#define PIN_SERIAL3_RX      (17)
+#define PIN_SERIAL3_TX      (16)
+#define PAD_SERIAL3_TX      (UART_TX_PAD_0)
+#define PAD_SERIAL3_RX      (SERCOM_RX_PAD_1)
+#define SERCOM_SERIAL3		  sercom1
 
 // Serial4
-#define PIN_SERIAL4_RX       (15)
-#define PIN_SERIAL4_TX       (14)
-#define PAD_SERIAL4_TX       (UART_TX_PAD_0)
-#define PAD_SERIAL4_RX       (SERCOM_RX_PAD_1)
-#define SERCOM_SERIAL4		 sercom5
+#define PIN_SERIAL4_RX      (15)
+#define PIN_SERIAL4_TX      (14)
+#define PAD_SERIAL4_TX      (UART_TX_PAD_0)
+#define PAD_SERIAL4_RX      (SERCOM_RX_PAD_1)
+#define SERCOM_SERIAL4		  sercom5
 /*
  * SPI Interfaces
  */
 #define SPI_INTERFACES_COUNT 2
 
-#define PIN_SPI_MISO         (64)
-#define PIN_SPI_MOSI         (66)
-#define PIN_SPI_SCK          (65)
-#define PERIPH_SPI           sercom7
-#define PAD_SPI_TX           SPI_PAD_0_SCK_1
-#define PAD_SPI_RX           SERCOM_RX_PAD_3
+#define PIN_SPI_MISO        (64)
+#define PIN_SPI_MOSI        (66)
+#define PIN_SPI_SCK         (65)
+#define PERIPH_SPI          sercom7
+#define PAD_SPI_TX          SPI_PAD_0_SCK_1
+#define PAD_SPI_RX          SERCOM_RX_PAD_3
 
 static const uint8_t SS	  = (53);
 static const uint8_t MOSI = PIN_SPI_MOSI;
 static const uint8_t MISO = PIN_SPI_MISO;
 static const uint8_t SCK  = PIN_SPI_SCK;
 
-#define PIN_SPI1_MISO         (80)
-#define PIN_SPI1_MOSI         (82)
-#define PIN_SPI1_SCK          (81)
-#define PIN_SPI1_SS           (83)
-#define PERIPH_SPI1           sercom2
-#define PAD_SPI1_TX           SPI_PAD_0_SCK_1
-#define PAD_SPI1_RX           SERCOM_RX_PAD_3
+#define PIN_SPI1_MISO       (80)
+#define PIN_SPI1_MOSI       (82)
+#define PIN_SPI1_SCK        (81)
+#define PIN_SPI1_SS         (83)
+#define PERIPH_SPI1         sercom2
+#define PAD_SPI1_TX         SPI_PAD_0_SCK_1
+#define PAD_SPI1_RX         SERCOM_RX_PAD_3
 
 static const uint8_t SS1	 = PIN_SPI1_SS;
 static const uint8_t MOSI1 = PIN_SPI1_MOSI;
@@ -202,29 +202,29 @@ static const uint8_t MISO1 = PIN_SPI1_MISO;
 static const uint8_t SCK1  = PIN_SPI1_SCK;
 
 // Needed for SD library
-#define SDCARD_SPI      SPI1
-#define SDCARD_MISO_PIN PIN_SPI1_MISO
-#define SDCARD_MOSI_PIN PIN_SPI1_MOSI
-#define SDCARD_SCK_PIN  PIN_SPI1_SCK
-#define SDCARD_SS_PIN   PIN_SPI1_SS
+#define SDCARD_SPI          SPI1
+#define SDCARD_MISO_PIN     PIN_SPI1_MISO
+#define SDCARD_MOSI_PIN     PIN_SPI1_MOSI
+#define SDCARD_SCK_PIN      PIN_SPI1_SCK
+#define SDCARD_SS_PIN       PIN_SPI1_SS
 
 /*
  * Wire Interfaces
  */
 #define WIRE_INTERFACES_COUNT 2
 
-#define PIN_WIRE_SDA         (62)
-#define PIN_WIRE_SCL         (63)
-#define PERIPH_WIRE          sercom3
-#define WIRE_IT_HANDLER      SERCOM3_Handler
+#define PIN_WIRE_SDA        (62)
+#define PIN_WIRE_SCL        (63)
+#define PERIPH_WIRE         sercom3
+#define WIRE_IT_HANDLER     SERCOM3_Handler
 
 static const uint8_t SDA = PIN_WIRE_SDA;
 static const uint8_t SCL = PIN_WIRE_SCL;
 
-#define PIN_WIRE1_SDA         (25)
-#define PIN_WIRE1_SCL         (24)
-#define PERIPH_WIRE1          sercom6
-#define WIRE1_IT_HANDLER      SERCOM6_Handler
+#define PIN_WIRE1_SDA       (25)
+#define PIN_WIRE1_SCL       (24)
+#define PERIPH_WIRE1        sercom6
+#define WIRE1_IT_HANDLER    SERCOM6_Handler
 
 static const uint8_t SDA1 = PIN_WIRE1_SDA;
 static const uint8_t SCL1 = PIN_WIRE1_SCL;
@@ -248,35 +248,35 @@ static const uint8_t SCL1 = PIN_WIRE1_SCL;
 #define PIN_I2S_SDI         (31)
 #define PIN_I2S_SCK         PIN_SERIAL4_TX
 #define PIN_I2S_FS          (33)
-#define PIN_I2S_MCK			PIN_SERIAL4_RX
+#define PIN_I2S_MCK			    PIN_SERIAL4_RX
 
 //QSPI Pins
-#define PIN_QSPI_SCK	(89)
-#define PIN_QSPI_CS		(90)
-#define PIN_QSPI_IO0	(91)
-#define PIN_QSPI_IO1	(92)
-#define PIN_QSPI_IO2	(93)
-#define PIN_QSPI_IO3	(94)
+#define PIN_QSPI_SCK	      (89)
+#define PIN_QSPI_CS		      (90)
+#define PIN_QSPI_IO0	      (91)
+#define PIN_QSPI_IO1	      (92)
+#define PIN_QSPI_IO2	      (93)
+#define PIN_QSPI_IO3	      (94)
 
 //PCC Pins
-#define PIN_PCC_DEN1    (26)
-#define PIN_PCC_DEN2    (27)
-#define PIN_PCC_CLK     (28)
-#define PIN_PCC_XCLK	  (29)
-#define PIN_PCC_D0      (37)
-#define PIN_PCC_D1      (36)
-#define PIN_PCC_D2      (35)
-#define PIN_PCC_D3      (34)
-#define PIN_PCC_D4      (33)
-#define PIN_PCC_D5      (32)
-#define PIN_PCC_D6      (31)
-#define PIN_PCC_D7      (30)
-#define PIN_PCC_D8      (39)
-#define PIN_PCC_D9      (38)
-#define PIN_PCC_D10     (41)
-#define PIN_PCC_D11     (40)
-#define PIN_PCC_D12     (43)
-#define PIN_PCC_D13     (42)
+#define PIN_PCC_DEN1        (26)
+#define PIN_PCC_DEN2        (27)
+#define PIN_PCC_CLK         (28)
+#define PIN_PCC_XCLK	      (29)
+#define PIN_PCC_D0          (37)
+#define PIN_PCC_D1          (36)
+#define PIN_PCC_D2          (35)
+#define PIN_PCC_D3          (34)
+#define PIN_PCC_D4          (33)
+#define PIN_PCC_D5          (32)
+#define PIN_PCC_D6          (31)
+#define PIN_PCC_D7          (30)
+#define PIN_PCC_D8          (39)
+#define PIN_PCC_D9          (38)
+#define PIN_PCC_D10         (41)
+#define PIN_PCC_D11         (40)
+#define PIN_PCC_D12         (43)
+#define PIN_PCC_D13         (42)
 
 #if !defined(VARIANT_QSPI_BAUD_DEFAULT)
   // TODO: meaningful value for this
