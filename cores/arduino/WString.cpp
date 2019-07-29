b076b0ae89d74bf2c09df97c041f3588e020d0e9
Jeff Rowberg <jeff@rowberg.net>
2019-07-29 12:03:20 -0400

    Add null pointer test to String destructor

--- cores/arduino/WString.cpp
+++ cores/arduino/WString.cpp
@@ -123,7 +123,7 @@ String::String(double value, unsigned char decimalPlaces)
 
 String::~String()
 {
-	free(buffer);
+	if (buffer) free(buffer);
 }
 
 /*********************************************/
