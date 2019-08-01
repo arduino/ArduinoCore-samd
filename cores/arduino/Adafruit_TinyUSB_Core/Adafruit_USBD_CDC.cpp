b88912c4cf4c5a27f0f045783fddca1c563d4746
hathach <thach@tinyusb.org>
2019-08-01 11:25:57 +0700

    fix availableForWrite() signature

--- cores/arduino/Adafruit_TinyUSB_Core/Adafruit_USBD_CDC.cpp
+++ cores/arduino/Adafruit_TinyUSB_Core/Adafruit_USBD_CDC.cpp
@@ -126,7 +126,7 @@ size_t Adafruit_USBD_CDC::write(const uint8_t *buffer, size_t size)
   return size - remain;
 }
 
-size_t Adafruit_USBD_CDC::availableForWrite(void)
+int Adafruit_USBD_CDC::availableForWrite(void)
 {
   return tud_cdc_write_available();
 }
