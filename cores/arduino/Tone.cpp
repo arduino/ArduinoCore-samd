1e9a669f07cce881c2861dfbc330cc0c5a0115d3
ladyada <ladyada>
2019-10-04 18:26:27 -0400

    change Tone on samd51 to be TC0 so we can use higher numbers for other stuff! (servo is TC1)

--- cores/arduino/Tone.cpp
+++ cores/arduino/Tone.cpp
@@ -37,9 +37,9 @@ volatile bool toneIsActive = false;
 volatile bool firstTimeRunning = false;
 
 #if defined(__SAMD51__)
-#define TONE_TC         TC2
-#define TONE_TC_IRQn    TC2_IRQn
-#define TONE_TC_GCLK_ID	TC2_GCLK_ID
+#define TONE_TC         TC0
+#define TONE_TC_IRQn    TC0_IRQn
+#define TONE_TC_GCLK_ID	TC0_GCLK_ID
 #else
 #define TONE_TC         TC5
 #define TONE_TC_IRQn    TC5_IRQn
@@ -48,7 +48,7 @@ volatile bool firstTimeRunning = false;
 #define TONE_TC_CHANNEL 0
 
 #if defined(__SAMD51__)
-void TC2_Handler (void) __attribute__ ((weak, alias("Tone_Handler")));
+void TC0_Handler (void) __attribute__ ((weak, alias("Tone_Handler")));
 #else
 void TC5_Handler (void) __attribute__ ((weak, alias("Tone_Handler")));
 #endif
@@ -83,7 +83,7 @@ void tone (uint32_t outputPin, uint32_t frequency, uint32_t duration)
     NVIC_SetPriority(TONE_TC_IRQn, 5);
 
 #if defined(__SAMD51__)
-	  GCLK->PCHCTRL[TONE_TC_GCLK_ID].reg = GCLK_PCHCTRL_GEN_GCLK0_Val | (1 << GCLK_PCHCTRL_CHEN_Pos);
+    GCLK->PCHCTRL[TONE_TC_GCLK_ID].reg = GCLK_PCHCTRL_GEN_GCLK0_Val | (1 << GCLK_PCHCTRL_CHEN_Pos);
 #else
     // Enable GCLK for TC4 and TC5 (timer counter input clock)
     GCLK->CLKCTRL.reg = (uint16_t) (GCLK_CLKCTRL_CLKEN | GCLK_CLKCTRL_GEN_GCLK0 | GCLK_CLKCTRL_ID(GCM_TC4_TC5));
