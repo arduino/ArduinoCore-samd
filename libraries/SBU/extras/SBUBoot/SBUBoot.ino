13b6e68ce5e16f422f403b67fcbae107b14fe06b
giulcioffi <g.cioffi@arduino.cc>
2020-07-09 15:54:18 +0200

    Add SBU for MKRNB1500 - initial commit

---
+++ libraries/SBU/extras/SBUBoot/SBUBoot.ino
@@ -0,0 +1,96 @@
+/*
+  Copyright (c) 2020 Arduino LLC.  All right reserved.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  See the GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include <FlashStorage.h>
+#include <MKRNB.h>
+
+#define SBU_START    0x2000
+#define SBU_SIZE     0x8000
+
+#define SKETCH_START (uint32_t*)(SBU_START + SBU_SIZE)
+
+static constexpr char UPDATE_FILE_NAME[] = "UPDATE.BIN";
+static constexpr char CHECK_FILE_NAME[] = "UPDATE.OK";
+
+FlashClass mcu_flash;
+
+NBFileUtils  fileUtils;
+
+extern "C" void __libc_init_array(void);
+
+int main()
+{
+  init();
+
+  __libc_init_array();
+
+  delay(1);
+
+  constexpr size_t blockSize = 512;
+  fileUtils.begin();
+  Serial1.begin(115200);
+  Serial1.print("SBU start. ");
+
+  bool update_success = false;
+
+  // Try to update only if update file
+  // has been download successfully.
+
+  if (fileUtils.listFile(CHECK_FILE_NAME)) {
+    Serial1.println("Update file exists");
+    uint32_t updateSize = fileUtils.listFile(UPDATE_FILE_NAME);
+    size_t cycles = (updateSize / blockSize) + 1;
+
+    if (updateSize > SBU_SIZE) {
+      updateSize = updateSize - SBU_SIZE - SBU_START;
+      /* Erase the MCU flash */
+      uint32_t flash_address = (uint32_t)SKETCH_START;
+      mcu_flash.erase((void*)flash_address, updateSize);
+
+      for (auto i = 0; i < cycles; i++) {
+        uint8_t block[blockSize] { 0 };
+        digitalWrite(LED_BUILTIN, LOW);
+        uint32_t read_bytes = fileUtils.readBlock(UPDATE_FILE_NAME, (i * blockSize) + SBU_SIZE + SBU_START, blockSize, block);
+        digitalWrite(LED_BUILTIN, HIGH);
+        mcu_flash.write((void*)flash_address, block, read_bytes);
+        flash_address += read_bytes;
+      }
+      update_success = true;
+    }
+    if (update_success) {
+      fileUtils.deleteFile(UPDATE_FILE_NAME);
+      fileUtils.deleteFile(CHECK_FILE_NAME);
+    }
+  }
+  else {
+    Serial1.println("Update file does not exist");
+    delay(100);
+  }
+
+boot:
+  /* Jump to the sketch */
+  __set_MSP(*SKETCH_START);
+
+  /* Reset vector table address */
+  SCB->VTOR = ((uint32_t)(SKETCH_START) & SCB_VTOR_TBLOFF_Msk);
+
+  /* Address of Reset_Handler is written by the linker at the beginning of the .text section (see linker script) */
+  uint32_t resetHandlerAddress = (uint32_t) * (SKETCH_START + 1);
+  /* Jump to reset handler */
+  asm("bx %0"::"r"(resetHandlerAddress));
+}
