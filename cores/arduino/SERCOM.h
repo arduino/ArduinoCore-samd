134ebe7e18b337ada134714d95e53d42dfa24fe7
Henry Gabryjelski <henrygab@users.noreply.github.com>
2020-08-06 16:11:10 -0700

    typo - missing semicolon

--- cores/arduino/SERCOM.h
+++ cores/arduino/SERCOM.h
@@ -248,7 +248,7 @@ class SERCOM
 		uint32_t getFreqRef(void) { return freqRef; };
 #else
 		// The equivalent SAMD21 dummy functions...
-		void setClockSource(int8_t idx, SercomClockSource src, bool core) { (void)idx; (void)src; (void)core };
+		void setClockSource(int8_t idx, SercomClockSource src, bool core) { (void)idx; (void)src; (void)core; };
 		SercomClockSource getClockSource(void) { return SERCOM_CLOCK_SOURCE_FCPU; };
 		uint32_t getFreqRef(void) { return F_CPU; };
 #endif
