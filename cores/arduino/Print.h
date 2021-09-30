6907751dd1910534140915eaacb87dca58d4825d
MartinL1 <31612615+MartinL1@users.noreply.github.com>
2021-09-30 12:25:26 +0100

    Update Print.h

--- cores/arduino/Print.h
+++ cores/arduino/Print.h
@@ -37,6 +37,7 @@ class Print
   private:
     int write_error;
     size_t printNumber(unsigned long, uint8_t);
+		size_t printULLNumber(unsigned long long, uint8_t);
     size_t printFloat(double, uint8_t);
   protected:
     void setWriteError(int err = 1) { write_error = err; }
@@ -69,6 +70,8 @@ class Print
     size_t print(unsigned int, int = DEC);
     size_t print(long, int = DEC);
     size_t print(unsigned long, int = DEC);
+		size_t print(long long, int = DEC);
+		size_t print(unsigned long long, int = DEC);
     size_t print(double, int = 2);
     size_t print(const Printable&);
 
@@ -81,6 +84,8 @@ class Print
     size_t println(unsigned int, int = DEC);
     size_t println(long, int = DEC);
     size_t println(unsigned long, int = DEC);
+		size_t println(long long, int = DEC);
+    size_t println(unsigned long long, int = DEC);
     size_t println(double, int = 2);
     size_t println(const Printable&);
     size_t println(void);
