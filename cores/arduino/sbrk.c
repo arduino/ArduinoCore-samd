4460dbc1412ff7f0f875669fa01e8a11fcb9ce58
Matthijs Kooijman <matthijs@stdin.nl>
2022-07-19 13:18:18 +0200

    Let malloc leave some extra space for the stack
    The _sbrk() implementation added previously refuses to overwrite the
    stack, but will still happily allocate all other memory, leaving none to
    expand the stack into. Since the stack can expand over the heap
    undetected, this changes sbrk() to leave a safety margin between the
    heap and the stack (at the time of the malloc/_sbrk call). The size of
    this margin can be configured (by the sketch) by changing the
    `__malloc_margin` global variable, it defaults to 64 bytes.
    
    This approach (both having a margin and making it configurable with the
    `__malloc_margin`) is copied from the avr-libc malloc implementation,
    see:
    
    https://onlinedocs.microchip.com/pr/GUID-317042D4-BCCE-4065-BB05-AC4312DBC2C4-en-US-2/index.html?GUID-27757112-8BE1-49C2-B023-CD94AD06C5E2
    
    avr-libc uses a default margin of 32 bytes, but given the bigger integer
    and pointer size on ARM, this is doubled for this core.

--- cores/arduino/sbrk.c
+++ cores/arduino/sbrk.c
@@ -2,13 +2,17 @@
 #include <stddef.h>
 #include <errno.h>
 
+/* How much free space to keep between the stack and the heap by malloc.
+ * Can be changed by the application at any time. */
+size_t __malloc_margin = 64;
+
 void *_sbrk(int incr)
 {
   extern char end; /* End of global variables, defined by the linker */
   static char *brkval = &end ;
   char *prev_brkval = brkval;
 
-  if (brkval + incr > (char *)__get_MSP()) {
+  if (brkval + incr + __malloc_margin > (char *)__get_MSP()) {
     /* Heap and stack collision */
     errno = ENOMEM;
     return (void*) -1;
