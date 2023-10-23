bdd0b63d7c1cb21745e095e0ad97c8d5f7edcf16
Maarten "merethan" van Eeuwijk <chef@edgewanderer.com>
2023-10-23 12:45:08 +0200

    Zeroing of section .bss is now conditional on .bss start & end being defined rather than .data start & end being defined (this was likely a copy-pasta error when re-purposing code from the .data section above it to make the .bss piece). Also, the pointer jumps forward 4 bytes at a time now rather than one byte, given that pSrc and pDest are pointers to a uint32_t, meaning memory is written 4 bytes at a time. A bunch of processor architectures (especially the simple ones) can only write int32 values to a memory address aligned at a 4-byte boundary, and int16 values aligned at a 2-byte boundary. If one writes C/C++ code not taking this into account, the compiler is forced to generate all sorts of shifts and tricks to make that code work, which is very suboptimal code by every measure. (In this case suboptimal means having every byte but the first and last three written 4 or more times over and some extra space in flash memory for the tricks.) If one were to do have written the init of .data and .bss in assembly (meaning the compiler not saves your ass by inserting shifts & tricks to your doing it per-byte) this would have been a HardFault on most Cortex CPUs due to the alignment requirement.

--- cores/arduino/cortex_handlers.c
+++ cores/arduino/cortex_handlers.c
@@ -146,13 +146,13 @@ void Reset_Handler(void)
   pDest = &__data_start__;
 
   if ((&__data_start__ != &__data_end__) && (pSrc != pDest)) {
-    for (; pDest < &__data_end__; pDest++, pSrc++)
+    for (; pDest < &__data_end__; pDest += sizeof(uint32_t), pSrc += sizeof(uint32_t))
       *pDest = *pSrc;
   }
 
   /* Clear the zero section */
-  if ((&__data_start__ != &__data_end__) && (pSrc != pDest)) {
-    for (pDest = &__bss_start__; pDest < &__bss_end__; pDest++)
+  if ((&__bss_start__ != &__bss_end__)) {
+    for (pDest = &__bss_start__; pDest < &__bss_end__; pDest += sizeof(uint32_t))
       *pDest = 0;
   }
 
